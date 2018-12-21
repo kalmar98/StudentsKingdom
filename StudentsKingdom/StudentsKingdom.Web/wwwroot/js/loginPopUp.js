df590f375445dd2901e73c3a8f2c11fe580bd1a7
Kaloyan Marinov <kalmar98softuni@gmail.com>
2018-12-22 01:15:28 +0200

    Updating the Index page and adding Login Form Pop Up. :)

---
+++ StudentsKingdom/StudentsKingdom.Web/wwwroot/js/loginPopUp.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+
+    var modalName = '#loginModal';
+
+    $('.play-btn').click(function () {
+        let url = $(modalName).data('url');
+        $.get(url, function (data) {
+            $(modalName).html(data);
+            $(modalName).modal('show');
+        });
+    });
+});
